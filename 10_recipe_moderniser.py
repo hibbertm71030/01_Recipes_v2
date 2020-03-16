4a7c0777505cb9958973e3ff697a916a85058515
hibbertm71030 <60630392+hibbertm71030@users.noreply.github.com>
2020-03-17 12:15:21 +1300

    fixed

---
+++ 10_recipe_moderniser.py
@@ -0,0 +1,16 @@
+# modules to be used...
+import csv
+
+# functions
+
+# main routine
+
+# set up Dictionaries
+
+# set up list to hold 'modernised' ingredients
+
+# Ask user for recipe name and check its not blank
+
+# get serving sizes and scale factor
+
+#
